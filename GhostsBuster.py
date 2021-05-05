4626187e4042c106ff0db17207edca0f98f03186
Lior Peled <liorpeledw@gmail.com>
2021-05-05 16:12:15 -0700

    changed the reading pages method - it does a much better job, but it is is sitll missing links. also please note that I need to clean up the two extra slashes other wise it won't work - also getting an out of bound error, need to work on that

--- GhostsBuster.py
+++ GhostsBuster.py
@@ -1,4 +1,5 @@
 import requests
+from requests_html import HTMLSession
 from urllib.request import urlparse, urljoin, urlopen
 from bs4 import BeautifulSoup
 import colorama
@@ -6,6 +7,7 @@ import colorama
 # init the colorama module
 colorama.init()
 invalidUrl = []
+visited = []
 
 
 def is_valid(url):
@@ -25,15 +27,25 @@ def is_valid(url):
 
 def get_all_links_in_webpage(url):
     urls = []
-
+    # domain name of the URL without the protocol
     domain_name = urlparse(url).netloc
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    # initialize an HTTP session
+    session = HTMLSession()
+    # make HTTP request & retrieve response
+    response = session.get(url)
+    # execute Javascript
+    try:
+        response.html.render()
+    except:
+        pass
+    soup = BeautifulSoup(response.html.html, "html.parser")
     for link in soup.findAll("a"):
-        if link.findAll(text=True)[0] != '\n':
-            text = link.findAll(text=True)[0]
-        else:
-            img = link.findAll("img")[0]
-            text = "https:" + img.attrs.get("src")
+        if(len(link.findAll(text=True)) > 0):
+            if link.findAll(text=True)[0] != '\n':
+                text = link.findAll(text=True)[0]
+            else:
+                img = link.findAll("img")[0]
+                text = "https:" + img.attrs.get("src")
 
         href = link.attrs.get("href")
         if href == "" or href is None:
@@ -42,20 +54,58 @@ def get_all_links_in_webpage(url):
         # join the URL if it's relative (not absolute link)
         href = urljoin(url, href)
         parsed_href = urlparse(href)
+
         # remove URL GET parameters, URL fragments, etc.
-        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+        if(str(parsed_href.netloc) == "fyberhelp.zendesk.com"):
+            home = "developer.fyber.com"
+        else:
+            home = parsed_href.netloc
+        href = parsed_href.scheme + "://" + home + parsed_href.path
+        temp = href.split()
+        href = ''.join(href.split())
+        print(href)
+        #print(text + " / " + href)
+        # need to check if link is an img
         item = (text, href)
-        urls.append(item)
+
+        if(url not in visited):
+            visited.append(url)
+        if(href not in visited):
+            urls.append(href)
+
     return urls
 
 
-def search(vals):
-    for i in vals.values():
-        newLinks = get_all_links_in_webpage(i[1])
-    return newLinks
+def crawl(url, max_urls=50):
+    """
+    Crawls a web page and extracts all links.
+    You'll find all links in `external_urls` and `internal_urls` global set variables.
+    params:
+        max_urls (int): number of max urls to crawl, default is 30.
+    """
+    print(url)
+    links = get_all_links_in_webpage(url)
+    print("inital visited")
+    print(visited)
+    print("printing links")
+    print(links)
+    for link in links:
+        print("printing url to crawl")
+        print(link)
+        print("printing the number of visited urls")
+        print(len(visited))
+        if len(visited) > max_urls:
+            break
+        crawl(link, max_urls=max_urls)
 
 
 if __name__ == "__main__":
-    links_in_page = get_all_links_in_webpage(
+    links = []
+    links = crawl(
         "https://developer.fyber.com/hc/en-us")
-    print(links_in_page)
+
+    # links = get_all_links_in_webpage(
+    #  "https://developer.fyber.com/hc/en-us")
+    for link in links:
+        # print(link)
+        break
